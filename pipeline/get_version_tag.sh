d17b8b920a7ed8ebc395223ecb4878ebfe3a56f2
Motaphe <suzalregmii@gmail.com>
2025-01-30 21:44:37 +0000

    test

--- pipeline/get_version_tag.sh
+++ pipeline/get_version_tag.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 # Use curl to fetch the data 
-get_response() {
-    response=$(curl -X GET \
-        "https://$DOCKER_REGISTRY/api/v2.0/projects/$DOCKER_PROJECT/repositories/$DOCKER_REPO/artifacts" \
-        -H 'accept: application/json' \
-        -H "authorization: Basic $HARBOR_USER_TOKEN")
-    echo "$response"
+get_response()
+{
+    response=$(curl -X 'GET'   \
+          "https://$DOCKER_REGISTRY/api/v2.0/projects/$DOCKER_PROJECT/repositories/$DOCKER_REPO/artifacts"\
+           -H 'accept: application/json'   -H "authorization: Basic $HARBOR_USER_TOKEN" )
+    echo $response
+}
+
+get_version()
+{
+    response=$(get_response)
+    # Check if the response is an empty array
+    if [[ "$response" == "[]" ]]; then
+        new_version="0.0.1"
+        echo "$new_version"
+    else
+        old_version=$(echo "$response" | jq -r '.[] | .tags[] | .name' | sort -V | tail -n 1 )
+        new_version=$(increment_version "$old_version")
+        echo "$new_version"
+
+    fi
+
 }
 
 increment_version() {
@@ -28,31 +44,6 @@ increment_version() {
 
     # Output the new version
     echo "$major.$minor.$patch"
-}
-
-get_version() {
-    response=$(get_response)
-
-    # Check if the response is literally an empty JSON array
-    if [[ "$response" == "[]" ]]; then
-        new_version="0.0.1"
-        echo "$new_version"
-    else
-        # Extract all tag names by searching for "name": "something"
-        old_version=$(echo "$response" \
-            | grep -o '"name":[[:space:]]*"[^"]*"' \
-            | cut -d'"' -f4 \
-            | sort -V \
-            | tail -n 1)
-
-        # If no tags found in the JSON, default to 0.0.1
-        if [ -z "$old_version" ]; then
-            new_version="0.0.1"
-        else
-            new_version=$(increment_version "$old_version")
-        fi
-        echo "$new_version"
-    fi
 }
 
 get_version
